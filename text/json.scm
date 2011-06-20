c715f764451dbafee42ab59701d042d4713526d2
Teppei Hamada <temada@gmail.com>
2011-06-21 01:04:12 +0900

    add license

--- text/json.scm
+++ text/json.scm
@@ -1,7 +1,36 @@
 ;;;
-;;; text.json - read/write json
+;;; text.json - read/write JSON
 ;;;
+;;;  Copyright (c) 2011  Teppei Hamada  <temada@gmail.com>
 ;;;
+;;;  Redistribution and use in source and binary forms, with or without
+;;;  modification, are permitted provided that the following conditions
+;;;  are met:
+;;;
+;;;  1. Redistributions of source code must retain the above copyright
+;;;     notice, this list of conditions and the following disclaimer.
+;;;
+;;;  2. Redistributions in binary form must reproduce the above copyright
+;;;     notice, this list of conditions and the following disclaimer in the
+;;;     documentation and/or other materials provided with the distribution.
+;;;
+;;;  3. Neither the name of the authors nor the names of its contributors
+;;;     may be used to endorse or promote products derived from this
+;;;     software without specific prior written permission.
+;;;
+;;;  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
 ;;; RFC4627  Javascript Object Notation (JSON)
 ;;; http://www.ietf.org/rfc/rfc4627
 
@@ -24,11 +53,13 @@
           ))
 (select-module text.json)
 
+
 ;; ---------------------------------------------------------
 ;; Conditions
 ;;
 (define-condition-type <json-read-error> <error> #f)
 (define-condition-type <json-write-error> <error> #f)
+
 
 ;; ---------------------------------------------------------
 ;; Assoc list wrapper class
